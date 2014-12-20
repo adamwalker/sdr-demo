d8f332c05cb92578b46b0645953d0f520828412b
Adam Walker <adamwalker10@gmail.com>
2014-12-20 15:32:15 +1300

    formatting

--- main.hs
+++ main.hs
@@ -84,30 +84,44 @@ doIt Options{..} = do
     audioSpectrum  <- plotFillAxes 1024 480 ((sqd `quot` 2) + 1) jet (zeroAxes 1024 480 48 10)
     pulseSink      <- lift $ pulseAudioSink 
 
-    let window0       = hanning samples :: VS.Vector CDouble
-        window        = hanning sqd
+    let window0 = hanning samples :: VS.Vector CDouble
+        window  = hanning sqd
 
     --Build the pipeline
-    let inputSpectrum :: Producer (VS.Vector (Complex CDouble)) IO ()
-        inputSpectrum = str >-> P.map (makeComplexBufferVect samples) 
+    let inputSpectrum ::  Producer (VS.Vector (Complex CDouble)) IO ()
+        inputSpectrum =   str 
+                      >-> P.map (makeComplexBufferVect samples) 
 
-        spectrumFFTSink :: Consumer (VS.Vector (Complex CDouble)) IO () 
-        spectrumFFTSink = P.map (VG.zipWith (flip mult) window0 . VG.zipWith mult (fftFixup samples)) >-> rfFFT >-> P.map (VG.map ((* (32 / fromIntegral samples)) . realToFrac . magnitude)) >-> rfSpectrum
+        spectrumFFTSink ::  Consumer (VS.Vector (Complex CDouble)) IO () 
+        spectrumFFTSink =   P.map (VG.zipWith (flip mult) window0 . VG.zipWith mult (fftFixup samples)) 
+                        >-> rfFFT 
+                        >-> P.map (VG.map ((* (32 / fromIntegral samples)) . realToFrac . magnitude)) 
+                        >-> rfSpectrum
 
         p1 :: Producer (VS.Vector (Complex CDouble)) IO () 
-        p1 = runEffect $ fork inputSpectrum >-> hoist lift spectrumFFTSink
+        p1 =  runEffect $   fork inputSpectrum 
+                        >-> hoist lift spectrumFFTSink
 
         demodulated :: Producer (VS.Vector CDouble) IO ()
-        demodulated = p1 >-> decimate decimation (VG.fromList coeffsRFDecim) samples sqd >-> P.map (fmDemodVec 0) >-> resample 3 10 (VG.fromList coeffsAudioResampler) sqd sqd >-> filterr (VG.fromList coeffsAudioFilter) sqd sqd
+        demodulated =   p1 
+                    >-> decimate decimation (VG.fromList coeffsRFDecim) samples sqd 
+                    >-> P.map (fmDemodVec 0) 
+                    >-> resample 3 10 (VG.fromList coeffsAudioResampler) sqd sqd 
+                    >-> filterr (VG.fromList coeffsAudioFilter) sqd sqd
 
         audioSpectrumSink :: Consumer (VS.Vector CDouble) IO ()
-        audioSpectrumSink = P.map (VG.zipWith (*) window) >-> audioFFT >-> P.map (VG.map ((/ 100) . realToFrac . magnitude)) >-> audioSpectrum
+        audioSpectrumSink =   P.map (VG.zipWith (*) window) 
+                          >-> audioFFT 
+                          >-> P.map (VG.map ((/ 100) . realToFrac . magnitude)) 
+                          >-> audioSpectrum
 
         p2 :: Producer (VS.Vector CDouble) IO ()
-        p2 = runEffect $ fork demodulated >-> hoist lift audioSpectrumSink
+        p2 =  runEffect $   fork demodulated 
+                        >-> hoist lift audioSpectrumSink
 
         audioSink :: Consumer (VS.Vector CDouble) IO ()
-        audioSink = P.map (VG.map ((* 0.2) . realToFrac)) >-> {-rate sqd >->-} pulseSink
+        audioSink =   P.map (VG.map ((* 0.2) . realToFrac)) 
+                  >-> {-rate sqd >->-} pulseSink
 
         pipeline :: IO ()
         pipeline = runEffect $ p2 >-> audioSink
